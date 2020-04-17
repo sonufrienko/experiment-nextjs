e88a00d8ab866ee783e0577d7dcbfb36a4787ef0
Sergey Onufrienko <sergey@onufrienko.com>
2020-04-17 10:39:35 +0100

    Add config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  target: 'serverless'
+  target: 'serverless',
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    GRAPHQL_API_URL: process.env.GRAPHQL_API_URL,
+    GRAPHQL_API_KEY: process.env.GRAPHQL_API_KEY
+  },
 };
