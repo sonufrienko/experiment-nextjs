1b4e634c2e17acc46f6b57146da4ece74fcfb2f9
Sergey Onufrienko <sergey@onufrienko.com>
2020-04-18 13:29:49 +0100

    Highlight menu items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,45 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Menu, Layout } from 'antd';
 import { HeatMapOutlined } from '@ant-design/icons';
 
-const Header = () => (
-  <Layout.Header>
-    <Link href="/">
-      <a className="logo">
-        <HeatMapOutlined className="icon" />
-        Real Places
-      </a>
-    </Link>
-    <Menu theme="dark" mode="horizontal">
-      <Menu.Item key="about">
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="contact">
-        <Link href="/contact">
-          <a>Contact Us</a>
-        </Link>
-      </Menu.Item>
-    </Menu>
-  </Layout.Header>
-);
+const menuItems = [
+  {
+    url: '/places/new',
+    title: 'Add place',
+  },
+  {
+    url: '/about',
+    title: 'About',
+  },
+  {
+    url: '/contact',
+    title: 'Contact Us',
+  },
+];
+
+const Header = () => {
+  const { route } = useRouter();
+
+  return (
+    <Layout.Header>
+      <Link href="/">
+        <a className="logo">
+          <HeatMapOutlined className="icon" />
+          Real Places
+        </a>
+      </Link>
+      <Menu theme="dark" mode="horizontal" selectedKeys={[route]}>
+        {menuItems.map((item) => (
+          <Menu.Item key={item.url}>
+            <Link href={item.url}>
+              <a>{item.title}</a>
+            </Link>
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Layout.Header>
+  );
+};
 
 export default Header;
